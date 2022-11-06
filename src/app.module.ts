b0d2cf294f029dc4e35b91156f54a3d657f6322b
matheus rizzi <matheusrizzi29@gmail.com>
2022-11-06 18:11:36 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { UsuarioModule } from './usuario/usuario.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './common/filtros/filtros-de-excecao-http.filter';
+import { TransformaRespostaInterceptor } from './core/http/transforma-resposta.interceptor';
+
+@Module({
+  imports: [UsuarioModule],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: FiltroDeExcecaoHttp,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformaRespostaInterceptor,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Uso de pipes para a validação dos dados
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  await app.listen(3000);
+}
+bootstrap();
